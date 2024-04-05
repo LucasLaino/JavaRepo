5ab6ede52df304018a754b905ca94958d97c0aa8
Lucas Laino de Andrade <lucaslaino00@gmail.com>
2024-04-05 19:38:32 -0300

    Novas Estruturas de controle.
    Adição do If Else, If Else If e Switch com e sem break.

---
+++ CursoJava/src/controle/IfElse.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class IfElse {
+
+    public static void main(String[] args){
+        double nota = 6.5;
+
+        if(nota >= 7){
+            System.out.println("Você foi Aprovado!");
+        } else {
+            System.out.println("Você está de Recuperação!");
+        }
+    }
+}
