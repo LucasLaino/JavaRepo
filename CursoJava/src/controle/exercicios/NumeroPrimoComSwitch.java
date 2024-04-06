a893b739bc46c0ff97ba64093ccbbba9e89ca973
Lucas Laino de Andrade <lucaslaino00@gmail.com>
2024-04-06 20:53:00 -0300

    Mais exercícios de estrutura de controle
    Adição do NumeroPositivo, NumeroPrimo e NumeroPrimoComSwitch

---
+++ CursoJava/src/controle/exercicios/NumeroPrimoComSwitch.java
@@ -0,0 +1,37 @@
+package controle.exercicios;
+
+import java.util.Scanner;
+
+public class NumeroPrimoComSwitch {
+	
+	/** Programa que recebe um número e fala se ele é um número primo utilizando a estrutura switch. */
+
+	public static void main(String[] args) {
+
+		int contadorDeDivisores = 0;
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("\nDigite um numero para verificar se é primo:");
+		int numero = scanner.nextInt();
+
+		for (int i = 2; i < numero; i++) {
+			if (numero % i == 0) {
+				contadorDeDivisores++;
+			}
+		}
+
+		switch (contadorDeDivisores) {
+
+		case 0:
+			System.out.println("O numero " + numero + " é um numero primo.");
+			break;
+
+		default:
+			System.out.println("O numero " + numero + "  não é um numero primo.");
+
+		}
+
+		scanner.close();
+
+	}
+}
